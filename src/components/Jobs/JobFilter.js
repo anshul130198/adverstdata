ec28e577a815edf7304e5f085e68c4a981985b3a
Anshul <anshul@your.hostname.com>
2022-08-16 14:40:33 +0530

    design improvement

--- src/components/Jobs/JobFilter.js
+++ src/components/Jobs/JobFilter.js
@@ -4,22 +4,64 @@ import Select from 'react-select'
 // import { colourOptions } from '../data';
 
 const initialValues = {
-  company: "",
-  position: "",
-  link: "",
-  date: "",
-  note: "",
+  ATS_NAME: "CLIENT_NAME",
+  POSTAL_CODE: "",
+  SURROUNDING_REGION: "",
+  DKZ: "",
+  JOB_TITLE: "",
+  JOB_POSTING: "",
+  EXCLUDE_EMPLOYMENT_AGENCIES: "",
+  WZ08_CODE: "",
+  JOB_CATEGORY_CODE: "",
+  LAND: "D",
+  COMPANY_ID: "",
+  COMPANY_NAME: "",
+  COMPANY_ID_LIST: "",
+  COMPANY_ID_LIST_EXCLUDE: "",
+  COMPANY_COUNTRY: "D",
+  COMPANY_POSTAL_CODE: "",
+  ITEM_FROM: "0",
+  NUMBER_OF_ITEMS: "1000",
+  DATE_FROM: "",
+  DATE_TO: "",
+  CONTAINS_AD_CONTACT_PERSON_SALUTATION: "",
+  CONTAINS_AD_CONTACT_PERSON_FIRSTNAME: "",
+  CONTAINS_AD_CONTACT_PERSON_LASTNAME: "",
+  CONTAINS_AD_CONTACT_PERSON_EMAIL: "",
+  AD_CONTACT_PERSON_EMAIL_CONTAINS_LASTNAME: "",
+  CONTAINS_AD_CONTACT_PERSON_TELEPHONE: "",
+  CAREER_LEVEL: "",
+  COMPANY_EMPLOYEES: "",
+  SIRET: "",
+  SEMANTIC: "",
+  POSITION_ID_LIST: "",
+  SORT_BY: "",
+  STATE: "",
+  WITHOUT_REAL_TIME_ADVERTS: "",
+  JOBS_GROUPED: "",
+  SORT_BY: "",
+  SORT_BY: "",
 };
 
 const JobFilter = () => {
   const [payload, setPayload] = useState(initialValues);
+  console.log('data', payload)
 
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setPayload({
+      ...payload,
+      [name]: value,
+    });
+  };
+
+  //  to be deleted
   const options = [
     { value: 'chocolate', label: 'Chocolate' },
     { value: 'strawberry', label: 'Strawberry' },
     { value: 'vanilla', label: 'Vanilla' }
   ]
-  
+
   const colourOptions = [
     { value: 'chocolate', label: 'Chocolate' },
     { value: 'strawberry', label: 'Strawberry' },
@@ -44,40 +86,29 @@ const JobFilter = () => {
       <h1>Filters</h1>
       <div className={styles["grid-container"]}>
         <div className={styles["grid-item"]}>
-          ATS Name <input className={styles["filterInput"]} type="text" value="Client Name" />
+          ATS Name <input className={styles["filterInput"]} name="ATS_NAME" type="text" value={payload.ATS_NAME} onChange={handleInputChange} />
         </div>
         <div className={styles["grid-item"]}>
-          POSTAL CODE <input className={styles["filterInput"]} type="text" value="" />
+          POSTAL CODE <input className={styles["filterInput"]} name="POSTAL_CODE" type="text" value={payload.POSTAL_CODE} onChange={handleInputChange} />
         </div>
         <div className={styles["grid-item"]}>
-          SURROUNDING REGION  <input className={styles["filterInput"]} type="number" value="" />
+          SURROUNDING REGION  <input className={styles["filterInput"]} name="SURROUNDING_REGION" type="number" value={payload.SURROUNDING_REGION} onChange={handleInputChange} />
         </div>
         <div className={styles["grid-item"]}>
-          DKZ <input className={styles["filterInput"]} type="text" value="Client Name" />
+          DKZ <input className={styles["filterInput"]} name="DKZ" type="text" value={payload.DKZ} onChange={handleInputChange} />
         </div>
         <div className={styles["grid-item"]}>
-          Job Title <input className={styles["filterInput"]} type="text" value="Client Name" />
+          Job Title <input className={styles["filterInput"]} name="JOB_TITLE" type="text" value={payload.JOB_TITLE} onChange={handleInputChange} />
         </div>
         <div className={styles["grid-item"]}>
-          Job Posting <input className={styles["filterInput"]} type="text" value="Client Name" />
+          Job Posting <input className={styles["filterInput"]} name="JOB_POSTING" type="text" value={payload.JOB_POSTING} onChange={handleInputChange} />
         </div>
         <div className={styles["grid-item"]}>
-          EXCLUDE EMPLOYMENT AGENCIES 
-          {/* <label htmlFor="cars">Select Company </label>
-          <select name="cars" id="cars">
-            <option value="Yes">yes</option>
-            <option value="No">no</option>
-          </select> */}
-            <Select options={booleanOptions} />
+          EXCLUDE EMPLOYMENT AGENCIES
+          <Select options={booleanOptions} />
         </div>
         <div className={styles["grid-item"]}>
           <label htmlFor="cars">Select Company </label>
-          {/* <select name="cars" id="cars">
-            <option value="volvo">Volvo</option>
-            <option value="saab">Saab</option>
-            <option value="mercedes">Mercedes</option>
-            <option value="audi">Audi</option>
-          </select> */}
           <Select
             defaultValue={[colourOptions[2], colourOptions[3]]}
             isMulti
@@ -85,137 +116,103 @@ const JobFilter = () => {
             options={colourOptions}
             className="basic-multi-select"
             classNamePrefix="select"
-         />
+          />
         </div>
         <div className={styles["grid-item"]}>
-          WZ08 CODE <input className={styles["filterInput"]} type="text" value="Client Name" />
+          WZ08 CODE <input className={styles["filterInput"]} name="WZ08_CODE" type="text" value={payload.WZ08_CODE} onChange={handleInputChange} />
         </div>
         <div className={styles["grid-item"]}>
-          JOB CATEGORY CODE <input className={styles["filterInput"]} type="text" />
+          JOB CATEGORY CODE <input className={styles["filterInput"]} name="LAND" value={payload.LAND} type="text" onChange={handleInputChange} />
         </div>
         <div className={styles["grid-item"]}>
-          LAND <input className={styles["filterInput"]} type="text" value="D" />
-        </div>
-        {/* <div className={styles["grid-item"]}>
-          COMPANY ID <input className={styles["filterInput"]} type="text" value="Client Name" />
-        </div> */}
-        <div className={styles["grid-item"]}>
-          COMPANY NAME <input className={styles["filterInput"]} type="text" value="Client Name" />
+          LAND <input className={styles["filterInput"]} name="name" type="text" value={payload.LAND} onChange={handleInputChange} />
         </div>
         <div className={styles["grid-item"]}>
-          COMPANY ID LIST <input className={styles["filterInput"]} type="text" value="Client Name" />
+          COMPANY ID <input className={styles["filterInput"]} type="text" name="COMPANY_ID" value={payload.COMPANY_ID} onChange={handleInputChange} />
         </div>
         <div className={styles["grid-item"]}>
-          COMPANY ID LIST EXCLUDE <input className={styles["filterInput"]} type="text" value="Client Name" />
+          COMPANY NAME <input className={styles["filterInput"]} type="text" name="COMPANY_NAME" value={payload.COMPANY_NAME} onChange={handleInputChange} />
         </div>
         <div className={styles["grid-item"]}>
-          COMPANY COUNTRY <input className={styles["filterInput"]} type="text" value="Client Name" />
+          COMPANY ID LIST <input className={styles["filterInput"]} type="text" name="COMPANY_ID_LIST" value={payload.COMPANY_ID_LIST} onChange={handleInputChange} />
         </div>
         <div className={styles["grid-item"]}>
-          COMPANY POSTAL CODE <input className={styles["filterInput"]} type="number" value="Client Name" />
+          COMPANY ID LIST EXCLUDE <input className={styles["filterInput"]} type="text" name="COMPANY_ID_LIST_EXCLUDE" value={payload.COMPANY_ID_LIST_EXCLUDE} onChange={handleInputChange} />
         </div>
         <div className={styles["grid-item"]}>
-        NUMBER OF ITEMS <input className={styles["filterInput"]} type="number" max="1000" value="1000" />
+          COMPANY COUNTRY <input className={styles["filterInput"]} type="text" name="COMPANY_COUNTRY" value={payload.COMPANY_COUNTRY} onChange={handleInputChange} />
         </div>
         <div className={styles["grid-item"]}>
-        DATE FROM <input className={styles["filterInput"]} type="date" value="Client Name" />
+          COMPANY POSTAL CODE <input className={styles["filterInput"]} type="number" name="COMPANY_POSTAL_CODE" value={payload.COMPANY_POSTAL_CODE} onChange={handleInputChange} />
         </div>
         <div className={styles["grid-item"]}>
-        DATE TO <input className={styles["filterInput"]} type="date" value="Client Name" />
+          ITEM FROM <input className={styles["filterInput"]} type="number" max="1000" name="ITEM_FROM" value={payload.ITEM_FROM} onChange={handleInputChange} />
+        </div>
+        <div className={styles["grid-item"]}>
+          NUMBER OF ITEMS <input className={styles["filterInput"]} type="number" max="1000" name="NUMBER_OF_ITEMS" value={payload.NUMBER_OF_ITEMS} onChange={handleInputChange} />
+        </div>
+        <div className={styles["grid-item"]}>
+          DATE FROM <input className={styles["filterInput"]} type="date" name="DATE_FROM" value={payload.DATE_FROM} onChange={handleInputChange} />
+        </div>
+        <div className={styles["grid-item"]}>
+          DATE TO <input className={styles["filterInput"]} type="date" name="DATE_TO" value={payload.DATE_TO} onChange={handleInputChange} />
         </div>
         <div className={styles["grid-item"]}>
           <label htmlFor="cars">CONTAINS AD CONTACT PERSON SALUTATION </label>
-          {/* <select name="cars" id="cars">
-            <option value="volvo">Yes</option>
-          </select> */}
-          <Select options={booleanOptions} />
+          <Select options={payload.CONTAINS_AD_CONTACT_PERSON_SALUTATION} />
         </div>
         <div className={styles["grid-item"]}>
           <label htmlFor="cars">CONTAINS AD CONTACT PERSON FIRSTNAME </label>
-          {/* <select name="cars" id="cars">
-            <option value="volvo">Yes</option>
-          </select> */}
           <Select options={booleanOptions} />
         </div>
         <div className={styles["grid-item"]}>
           <label htmlFor="cars">CONTAINS AD CONTACT PERSON LASTNAME </label>
-          {/* <select name="cars" id="cars">
-            <option value="volvo">Yes</option>
-          </select> */}
-            <Select options={booleanOptions} />
+          <Select options={booleanOptions} />
         </div>
         <div className={styles["grid-item"]}>
           <label htmlFor="cars">CONTAINS AD CONTACT PERSON EMAIL </label>
-          {/* <select name="cars" id="cars">
-            <option value="volvo">Yes</option>
-          </select> */}
           <Select options={booleanOptions} />
         </div>
         <div className={styles["grid-item"]}>
           <label htmlFor="cars">AD CONTACT PERSON EMAIL CONTAINS LASTNAME </label>
-          {/* <select name="cars" id="cars">
-            <option value="volvo">Yes</option>
-          </select> */}
           <Select options={booleanOptions} />
         </div>
         <div className={styles["grid-item"]}>
           <label htmlFor="cars">CONTAINS AD CONTACT PERSON TELEPHONE </label>
-          {/* <select name="cars" id="cars">
-            <option value="volvo">Yes</option>
-          </select> */}            
           <Select options={booleanOptions} />
         </div>
-          <div className={styles["grid-item"]}>
-          CAREER LEVEL <input className={styles["filterInput"]} type="text" value="D" />
-          </div>
-          <div className={styles["grid-item"]}>
-          COMPANY EMPLOYEES <input className={styles["filterInput"]} type="text" value="D" />
-          </div>
-          <div className={styles["grid-item"]}>
-          SIRET <input className={styles["filterInput"]} type="text" value="D" />
-          </div>
+        <div className={styles["grid-item"]}>
+          CAREER LEVEL <input className={styles["filterInput"]} name="CAREER_LEVEL" type="text" value={payload.CAREER_LEVEL} onChange={handleInputChange} />
+        </div>
+        <div className={styles["grid-item"]}>
+          COMPANY EMPLOYEES <input className={styles["filterInput"]} name="COMPANY_EMPLOYEES" type="text" value={payload.COMPANY_EMPLOYEES} onChange={handleInputChange} />
+        </div>
+        <div className={styles["grid-item"]}>
+          SIRET <input className={styles["filterInput"]} name="SIRET" type="text" value={payload.SIRET} onChange={handleInputChange} />
+        </div>
         <div className={styles["grid-item"]}>
           <label htmlFor="cars">SEMANTIC </label>
-          {/* <select name="cars" id="cars">
-            <option value="volvo">Yes</option>
-          </select> */}
           <Select options={booleanOptions} />
         </div>
         <div className={styles["grid-item"]}>
           <label htmlFor="cars">POSITION ID LIST </label>
-          {/* <select name="cars" id="cars">
-            <option value="volvo">Yes</option>
-          </select> */}
           <Select options={booleanOptions} />
         </div>
         <div className={styles["grid-item"]}>
           <label htmlFor="sortBy">Sort </label>
-          {/* <select name="sortBy" id="sortBy">
-            <option value="Date">Date</option>
-            <option value="ASC">Date ASC</option>
-            <option value="DESC">Date DESC</option>
-            <option value="POSITION">Position</option>
-            <option value="COMPANY">Company</option>
-          </select> */}
-          <Select options={sortOptions}/>
+          <Select options={sortOptions} />
         </div>
         <div className={styles["grid-item"]}>
           <label htmlFor="cars">WITHOUT REAL TIME ADVERTS </label>
-          {/* <select name="cars" id="cars">
-            <option value="volvo">Yes</option>
-          </select> */}
           <Select options={booleanOptions} />
         </div>
         <div className={styles["grid-item"]}>
           <label htmlFor="cars">JOBS GROUPED </label>
-          {/* <select name="cars" id="cars">
-            <option value="volvo">Yes</option>
-          </select> */}
           <Select options={booleanOptions} />
         </div>
       </div>
     </div>
-  );
+  )
 };
 
 export default JobFilter;
--- src/components/Jobs/JobList.js
+++ src/components/Jobs/JobList.js
@@ -4,7 +4,7 @@ import CommonService from "../../services/Common";
 import strings from "../../utils/localization/localization";
 import to from "../../utils/to";
 import JobFilter from "./JobFilter";
-import JobListItem from "./JobListItem";
+// import JobListItem from "./JobListItem";
 import styles from './JobList.module.scss'
 
 const Jobs = () => {
@@ -15,7 +15,7 @@ const Jobs = () => {
   const handleSearch = async (e) => {
     e.preventDefault()
     setLoading(true)
-    const [error, res] = await to(commonService.getPosts())
+    const [error, res] = await to(commonService.filterData())
     console.log('res', res)
     if (error) {
       console.log('nope')
@@ -27,20 +27,20 @@ const Jobs = () => {
   }
   return (
     <div>
-      <h1>{strings.JOBS}</h1>
-      <form action="" className={styles["form"]}>
+      {/* <h1>{strings.JOBS}</h1> */}
+      {/* <form action="" className={styles["form"]}>
         <input type="text" />
-        <button onClick={handleSearch}>
-          search
-        </button>
-      </form>
+      </form> */}
       {<JobFilter />}
       {loading && <Loading />}
       {/* {
         jobs?.length && !loading
-          ? jobs.map((item) => <JobListItem job={item} key={item.id} />)
-          : <p>search to get data</p>
+        ? jobs.map((item) => <JobListItem job={item} key={item.id} />)
+        : <p>search to get data</p>
       } */}
+      <button type="submit" onClick={handleSearch}>
+        search
+      </button>
     </div>
   );
 };
